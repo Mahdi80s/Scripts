59121475f99c30e692904ef69a6da5f312be75b8
Mahdi Sardari <mahdisardari80@outlook.com>
2024-10-31 19:58:31 +0330

    Update

--- Exercise4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for number in {5..50..5};
-do 
-   echo $number
-done
--- Exercise5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-OUTPUT_PATH="/home/ubuntu"
-
-awk -F":" '{print $1 ";" $3}' /etc/passwd >> $OUTPUT_PATH/exercise5_`date +"%Y%m%d"`.log
-
-find $OUTPUT_PATH -name "exercise5*.log" -mtime +2 -exec rm {} \;
